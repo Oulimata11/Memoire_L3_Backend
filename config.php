88f96cf7f7ceca3f9a575a1665d62d95a91793f7
Amarmouhamed <amarmouhamed4@gmail.com>
2022-07-28 15:05:31 +0000

    version 1.0 complete

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+//à commenter en mode production pour imposer les remquetes de même provenance
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
